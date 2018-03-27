c643db19c3c333320eaf31a15d52a10e1ece31e8
Stanislaw Bogatkin <sbog@sbog.ru>
2018-03-28 00:55:29 +0300

    Add base iptables role
    Add base ansible role to install and configure iptables.

---
+++ README.md
@@ -0,0 +1,51 @@
+sbog/iptables
+=============
+
+Role to install and configure iptables
+
+#### Requirements
+
+Ansible 2.4
+
+#### Role Variables
+
+```yaml
+iptables:
+  # Enable whole role
+  enabled: yes
+  # Flush all rules before adding role ones
+  flush_all: yes
+  # Allow NTP
+  ntp_allowed: yes
+  # List of allowed TCP ports
+  allowed_tcp_ports: [22, 80, 443]
+  # List of allowed UDP ports
+  allowed_udp_ports: []
+  # Any raw rules can be added
+  raw_rules: []
+  # Deny all which were not explicitly allowed
+  deny_all_unallowed: yes
+```
+
+#### Dependencies
+
+None
+
+#### Example Playbook
+
+```yaml
+- name: Run and configure Iptables
+  hosts: all
+  remote_user: root
+
+  roles:
+    - iptables
+```
+
+#### License
+
+Apache 2.0
+
+#### Author Information
+
+Stanislaw Bogatkin (https://sbog.ru)
---
+++ templates/rules/rules.generator.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+IPTABLES=/sbin/iptables
+
+{% if iptables.flush_all %}
+# Clean all
+$IPTABLES -F
+$IPTABLES -X
+$IPTABLES -t nat -F
+$IPTABLES -t nat -X
+$IPTABLES -t mangle -F
+$IPTABLES -t mangle -X
+{% endif %}
+
+# Accept all from localhost
+$IPTABLES -A INPUT -i lo -j ACCEPT
+
+# Allow established connections:
+$IPTABLES -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# Accept ICMP packets (it allows ping for example)
+{% if iptables.icmp_allowed %}
+$IPTABLES -A INPUT -p icmp -j ACCEPT
+{% endif %}
+
+# Allow NTP traffic for time synchronization.
+{% if iptables.ntp_allowed %}
+$IPTABLES -A OUTPUT -p udp --dport 123 -j ACCEPT
+$IPTABLES -A INPUT -p udp --sport 123 -j ACCEPT
+{% endif %}
+
+# Allowed ports.
+{% for port in iptables.allowed_tcp_ports %}
+$IPTABLES -A INPUT -p tcp -m tcp --dport {{port}} -j ACCEPT
+{% endfor %}
+{% for port in iptables.allowed_udp_ports %}
+$IPTABLES -A INPUT -p udp -m udp --dport {{port}} -j ACCEPT
+{% endfor %}
+
+# Raw rules
+{% for rule in iptables.raw_rules %}
+$IPTABLES {{rule}}
+{% endfor %}
+
+# Drop all other
+{% if iptables.deny_all_unallowed %}
+$IPTABLES -A INPUT -j DROP
+{% endif %}
