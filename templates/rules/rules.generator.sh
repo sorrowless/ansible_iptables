215cc81470ffb3e76452a89198d4134d5cda0eff
Stanislaw Bogatkin <sbog@sbog.ru>
2018-03-29 18:54:14 +0300

    Make rules generator more idempotent

--- templates/rules/rules.generator.sh
+++ templates/rules/rules.generator.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 IPTABLES=/sbin/iptables
 
@@ -12,46 +12,60 @@ $IPTABLES -t mangle -F
 $IPTABLES -t mangle -X
 {% endif %}
 
+function idempotent_add {
+  RULE="$1"
+  echo "Check rule $RULE"
+  iptables-save | grep -- "$RULE"
+  RC=$?
+  if [[ $RC == 0 ]]; then
+    echo "That rule already exists in rules list"
+  else
+    echo "That rule doesn't exists in rules list, add it"
+    $IPTABLES $RULE
+  fi
+  return $RC
+}
+
 # Accept all from localhost
-$IPTABLES -A INPUT -i lo -j ACCEPT
+idempotent_add '-A INPUT -i lo -j ACCEPT'
 
 # Allow established connections:
-$IPTABLES -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+idempotent_add '-A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT'
 
 # Accept ICMP packets (it allows ping for example)
 {% if iptables.icmp_allowed %}
-$IPTABLES -A INPUT -p icmp -j ACCEPT
+idempotent_add '-A INPUT -p icmp -j ACCEPT'
 {% endif %}
 
 # Allow NTP traffic for time synchronization.
 {% if iptables.ntp_allowed %}
-$IPTABLES -A OUTPUT -p udp --dport 123 -j ACCEPT
-$IPTABLES -A INPUT -p udp --sport 123 -j ACCEPT
+idempotent_add '-A OUTPUT -p udp -m udp --dport 123 -j ACCEPT'
+idempotent_add '-A INPUT -p udp -m udp --sport 123 -j ACCEPT'
 {% endif %}
 
 # Allowed ports.
 {% for port in iptables.default_allowed_tcp_ports %}
-$IPTABLES -A INPUT -p tcp -m tcp --dport {{port}} -j ACCEPT
+idempotent_add '-A INPUT -p tcp -m tcp --dport {{port}} -j ACCEPT'
 {% endfor %}
 {% for port in iptables.group_allowed_tcp_ports %}
-$IPTABLES -A INPUT -p tcp -m tcp --dport {{port}} -j ACCEPT
+idempotent_add '-A INPUT -p tcp -m tcp --dport {{port}} -j ACCEPT'
 {% endfor %}
 {% for port in iptables.host_allowed_tcp_ports %}
-$IPTABLES -A INPUT -p tcp -m tcp --dport {{port}} -j ACCEPT
+idempotent_add '-A INPUT -p tcp -m tcp --dport {{port}} -j ACCEPT'
 {% endfor %}
 {% for port in iptables.default_allowed_udp_ports %}
-$IPTABLES -A INPUT -p udp -m udp --dport {{port}} -j ACCEPT
+idempotent_add '-A INPUT -p udp -m udp --dport {{port}} -j ACCEPT'
 {% endfor %}
 {% for port in iptables.group_allowed_udp_ports %}
-$IPTABLES -A INPUT -p udp -m udp --dport {{port}} -j ACCEPT
+idempotent_add '-A INPUT -p udp -m udp --dport {{port}} -j ACCEPT'
 {% endfor %}
 {% for port in iptables.host_allowed_udp_ports %}
-$IPTABLES -A INPUT -p udp -m udp --dport {{port}} -j ACCEPT
+idempotent_add '-A INPUT -p udp -m udp --dport {{port}} -j ACCEPT'
 {% endfor %}
 
 # Raw rules
 {% for rule in iptables.raw_rules %}
-$IPTABLES {{rule}}
+idempotent_add '{{rule}}'
 {% endfor %}
 
 # Drop all other
